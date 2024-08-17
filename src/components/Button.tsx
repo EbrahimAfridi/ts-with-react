6f0f00571ac8faf811cee565f3978024876973a8
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-17 23:17:31 +0530

    learned how to make prop types

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+// Make a seprate type to reduce repetition
+type ButtonProps = {
+  text: string;
+  bgColor: string;
+  textColor: string;
+};
+const Button = (props: ButtonProps) => {
+  const { text, bgColor, textColor } = props;
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: bgColor, color: textColor }}
+        className="rounded px-3 py-2 absolute top-1/2 left-1/2"
+      >
+        click me! {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;

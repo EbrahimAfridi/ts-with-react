e7bf63441f0682ed6855b82fc748f6752791c421
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-17 23:38:16 +0530

    learned how to add tuple and array of types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <main className="bg-zinc-800 h-screen">
-      <Button text="Hello" bgColor="red" textColor="white" paddingArray={[10, 5, 20, 4]}/>
+      <Button
+        text="Hello"
+        bgColor="red"
+        textColor="white"
+        paddingArray={["10px ", "5px ", "10px ", "5px "]}
+        marginTuple={[10, 5]}
+      />
     </main>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,14 +8,20 @@ type ButtonProps = {
   text: string;
   bgColor: Color;
   textColor?: string;
-  paddingArray: number[];
+  paddingArray: string[];
+  marginTuple?: [number, number];
 };
 const Button = (props: ButtonProps) => {
-  const { text, bgColor, textColor } = props;
+  const { text, bgColor, textColor, paddingArray, marginTuple } = props;
   return (
     <div>
       <button
-        style={{ backgroundColor: bgColor, color: textColor }}
+        style={{
+          backgroundColor: bgColor,
+          color: textColor,
+          padding: `${paddingArray[0]} ${paddingArray[1]} ${paddingArray[2]} ${paddingArray[3]}`,
+          margin: `${marginTuple ? marginTuple[0] : 0}px ${marginTuple ? marginTuple[1] : 0}px`,
+        }}
         className="rounded px-3 py-2 absolute top-1/2 left-1/2"
       >
         click me! {text}
